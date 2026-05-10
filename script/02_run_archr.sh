a83dda612bc7596b96b0de9007941112d20ad7e9
liona-vu <230396061+liona-vu@users.noreply.github.com>
2026-05-10 18:53:39 -0400

    Create 02_run_archr.sh

---
+++ script/02_run_archr.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#change time, mem, and cpu as appropriate to batch jobs submissions
+
+#SBATCH --job-name=02_run_archr
+#SBATCH --account=def-itobias
+#SBATCH --time=00:10:00
+#SBATCH --mem=2G
+#SBATCH --cpus-per-task=1
+#SBATCH --error=%x_%j_error.txt
+#SBATCH --output=%x_%j_output.txt
+
+INPUT_RSCRIPT=${1}
+
+#load module
+module load apptainer/1.4.5
+
+#run rscript
+apptainer exec archr_image.sif Rscript $INPUT_RSCRIPT
