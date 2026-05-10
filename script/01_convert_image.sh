4394bcbbae24b97bd14327ffc95ade4669cfcc11
liona-vu <230396061+liona-vu@users.noreply.github.com>
2026-05-10 13:02:54 -0400

    Update 01_convert_image.sh

--- script/01_convert_image.sh
+++ script/01_convert_image.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+#SBATCH --job-name=download_archr
+#SBATCH --account=def-itobias
+#SBATCH --time=00:10:00
+#SBATCH --mem=10xG
+#SBATCH --cpus-per-task=1
+#SBATCH --error=%x_%j_error.txt
+#SBATCH --output=%x_%j_output.txt
+
+#load module
+module load apptainer/1.4.5
+
+#convert archr docker image to sif image for apptainer
+apptainer pull docker://immanuelazn/archr:latest
