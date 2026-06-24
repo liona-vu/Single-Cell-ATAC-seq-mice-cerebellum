c5c01058dec745197316b4571233198df71b8842
liona-vu <230396061+liona-vu@users.noreply.github.com>
2026-06-23 22:27:46 -0400

    Update 02_run_archr.sh

--- script/02_run_archr.sh
+++ script/02_run_archr.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-#change time, mem, and cpu as appropriate to batch jobs submissions
-
-#SBATCH --job-name=02_run_archr
+#SBATCH --job-name=run_archr
 #SBATCH --account=def-itobias
+#SBATCH --time=4:00:00
+#SBATCH --mem=16384M
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
-#SBATCH --time=00:10:00
-#SBATCH --mem=2G
-#SBATCH --error=%x_%j_error.txt
-#SBATCH --output=%x_%j_output.txt
+#SBATCH --cpus-per-task=1          #threading parellelization
+##SBATCH --error=%x_%j_error.txt
+##SBATCH --output=%x_%j_output.txt
 
-#create positional parameter to input script
-INPUT_RSCRIPT=${1}
+#create positional parameters to input sif image and script
+ARCHR_IMAGE=${1}
+INPUT_RSCRIPT=${2}
 
 #load module
 module load apptainer/1.4.5
+#module load r/4.5.0
 
 #run rscript
-apptainer exec archr_image.sif Rscript $INPUT_RSCRIPT
+apptainer exec $ARCHR_IMAGE Rscript $INPUT_RSCRIPT
 
 
-#for an interactive session run this instead
-apptainer shell archr_image.sif
+#for an interactive session on the HPC, run this instead
+#apptainer shell archr_image.sif
 
 #then run 
-R
+#R
